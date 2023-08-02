59030df65f7e149a550a991a66fe767c99db92b0
TapanBaral <tbaral666@gmail.com>
2023-08-02 08:09:58 +0800

    initial commit

--- R/init.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  if(!requireNamespace("monocle3", quietly = TRUE)) {
-    install.packages("remotes")
-    remotes::install_github('cole-trapnell-lab/monocle3')
-  }
-}
---
+++ install-packages.R
@@ -0,0 +1,4 @@
+#!/usr/bin/env Rscript
+
+# devtools::install_github(lib='',repo = 'satijalab/seurat', ref = 'release/3.0', force=T)
+devtools::install_github("satijalab/seurat", "seurat5", quiet = TRUE, force = T)
