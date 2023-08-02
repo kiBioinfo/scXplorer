e364e14265767abc16fc774806126b5f7c281214
TapanBaral <tbaral666@gmail.com>
2023-08-03 00:50:56 +0800

    exec folder added

---
+++ exec/sce_run.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript --vanilla
+
+##Check to see if necessary packages are installed
+#CRAN packages
+cran.packages <- c("optparse", "yaml", "igraph", "Rtsne")
+
+cran.package.check <- lapply(cran.packages, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
+  }
+})
+
+#Bioconductor packages
+bioc.packages <- c("singleCellTK",  "BiocParallel")
+
+bioc.package.check <- lapply(bioc.packages, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    BiocManager::install(x)
+  }
+})
