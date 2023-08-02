d5ac43d49aa24f2945d8377a5e8ae68ce24bb7ea
TapanBaral <tbaral666@gmail.com>
2023-08-02 09:07:30 +0800

    modified

--- install-packages.R
+++ install-packages.R
@@ -2,3 +2,41 @@
 
 devtools::install_github("satijalab/seurat", "seurat5", quiet = TRUE, force = T)
 #devtools::install_github(lib='',repo = 'cole-trapnell-lab/monocle3' , ref = 'release/1.3.1',  force = T, dependencies = T)
+
+
+if(!require(shiny)){
+  install.packages("shiny")
+  library(shiny)
+}
+if(!require(shinyjs)){
+  install.packages("shinyjs")
+  library(shinyjs)
+}
+if(!require(shinyBS)){
+  install.packages("shinyBS")
+  library(shinyBS)
+}
+if(!require(shinycssloaders)){
+  install.packages("shinycssloaders")
+  library(shinycssloaders)
+}
+if(!require(DT)){
+  install.packages("DT")
+  library(DT)
+}
+if(!require(dplyr)){
+  install.packages("dplyr")
+  library(dplyr)
+}
+if(!require(Matrix)){
+  install.packages("Matrix")
+  library(Matrix)
+}
+
+
+
+
+if(!require(ggplot2)){
+  install.packages("ggplot2")
+  library(ggplot2)
+}
