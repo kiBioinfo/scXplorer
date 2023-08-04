9c0319bf1d497e35bd302b0a56fb34b3d77c4c8d
TapanBaral <tbaral666@gmail.com>
2023-08-05 04:46:43 +0800

    modified

--- install-packages.R
+++ install-packages.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-devtools::install_github("satijalab/seurat", "seurat5", quiet = TRUE, force = T)
+# devtools::install_github("satijalab/seurat", "seurat5", quiet = TRUE, force = T)
 #devtools::install_github(lib='',repo = 'cole-trapnell-lab/monocle3' , ref = 'release/1.3.1',  force = T, dependencies = T)
 devtools::install_github('cole-trapnell-lab/monocle3', force = T, dependencies = T,build = T)
 
